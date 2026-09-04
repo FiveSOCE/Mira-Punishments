7de49412530e34066bdd8dce55f199c59a33d998
FiveS <sgmmsionide@gmail.com>
2026-09-04 19:21:30 +1000

    Add punishment-issued lifecycle event

---
+++ src/main/java/gg/mira/punishments/api/event/PunishmentIssuedEvent.java
@@ -0,0 +1,20 @@
+package gg.mira.punishments.api.event;
+
+import gg.mira.punishments.MiraPunishmentsPlugin.Punishment;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public final class PunishmentIssuedEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final Punishment punishment;
+
+    public PunishmentIssuedEvent(Punishment punishment) {
+        this.punishment = punishment;
+    }
+
+    public Punishment punishment() { return punishment; }
+
+    @Override public @NotNull HandlerList getHandlers() { return HANDLERS; }
+    public static @NotNull HandlerList getHandlerList() { return HANDLERS; }
+}
